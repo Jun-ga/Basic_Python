f41e8070d09af8957e9f13220a8189ac51fea49d
gek7537 <56713634+gek7537@users.noreply.github.com>
2020-01-07 04:01:32 +0900

    Create Operators_2_CJA.py

---
+++ Day_2/Operators_2_CJA.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+
+    totalCost = meal_cost + (meal_cost * tip_percent / 100) + (meal_cost * tax_percent / 100)
+    totalCost_ = round(totalCost)
+    print (int(totalCost_))
+
+
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
+
