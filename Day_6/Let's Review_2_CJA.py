aac15686dce01cb87addc42ebcbf3ed5f9f81d89
gek7537 <56713634+gek7537@users.noreply.github.com>
2020-01-07 04:18:12 +0900

    Create Let's Review_2_CJA.py

---
+++ Day_6/Let's Review_2_CJA.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+for t in range(n):
+    s = input()
+    s1 = ""
+    s2 = ""
+    for i in range(len(s)):
+        if i%2==0:
+            s1 += s[i]
+        else:
+            s2 += s[i]
+    print(s1, s2)
