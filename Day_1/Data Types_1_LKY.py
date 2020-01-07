b407ae30d250cdc126ddfe3373d5cb4653d3c393
kwonjunhee <55957124+kwonjunhee@users.noreply.github.com>
2020-01-07 11:33:52 +0900

    Create Data Types_1_LKY.py

---
+++ Day_1/Data Types_1_LKY.py
@@ -0,0 +1,9 @@
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+# Print the sum of both integer variables on a new line.
+# Print the sum of the double variables on a new line.
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(int(input()) + i)
+print(float(input()) + d)
+print(s + input())
