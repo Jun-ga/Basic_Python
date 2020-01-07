dee3f5eeb3c9a4c68b8d0e2c1b1fe964aacdc821
kwonjunhee <55957124+kwonjunhee@users.noreply.github.com>
2020-01-07 11:38:17 +0900

    Create Data Types_1_PJH.py

---
+++ Day_1/Data Types_1_PJH.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+a=int(input())
+f=float(input())
+ss=input()
+print(a+i)
+print(f+d)
+print(s+ss)
