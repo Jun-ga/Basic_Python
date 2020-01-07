ac6a868e00f2a8fc592e3ff789dc11a1346a0f3b
kwonjunhee <55957124+kwonjunhee@users.noreply.github.com>
2020-01-07 11:36:56 +0900

    Create Arrays_1_LKY.py

---
+++ Day_7/Arrays_1_LKY.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    result_list = []
+    for index in range(n):
+        result_list.append(arr[n - index - 1])
+    print(*result_list)
