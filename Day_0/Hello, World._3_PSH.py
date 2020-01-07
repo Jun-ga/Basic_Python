3ce8cd8c9d4bb2aa5d063db936ccfdfe766787da
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2020-01-07 16:16:37 +0900

    Create Hello, World._3_PSH.py

---
+++ Day_0/Hello, World._3_PSH.py
@@ -0,0 +1,9 @@
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System); 
+		String inputString = scan.nextLine(); 
+		scan.close(); 
+        println("Hello, World.");
+        println( inputString ); 
+	}
+}
