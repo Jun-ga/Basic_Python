73ed425948a2fd2cf285b97692aae8f42642551b
kwonjunhee <55957124+kwonjunhee@users.noreply.github.com>
2020-01-07 11:36:02 +0900

    Create Loops_1_LKY.py

---
+++ Day_5/Loops_1_LKY.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+if __name__ == '__main__':
+    n = int(input())
+    for number in range(10):
+        print(str(n) + ' x ' + str(number + 1) + ' = ' + str(n * (number + 1)))
