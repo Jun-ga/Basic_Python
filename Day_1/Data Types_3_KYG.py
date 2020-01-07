1513585c0a060ece4144fc16e22d31df5d083c54
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2020-01-07 16:23:19 +0900

    Create Data Types_3_KYG.py

---
+++ Day_1/Data Types_3_KYG.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+j=int(input(''))
+e=float(input(''))
+# Read and save an integer, double, and String to your variables.
+# Print the sum of both integer variables on a new line.
+a= input('')
+# Print the sum of the double variables on a new line.
+print(i+j)
+print(d+e)
+print(s+a)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
