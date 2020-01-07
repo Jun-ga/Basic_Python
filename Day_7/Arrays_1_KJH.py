fffda92800af5e85272fd851e6d98c3869a84e7f
kwonjunhee <55957124+kwonjunhee@users.noreply.github.com>
2020-01-07 11:47:40 +0900

    Create Arrays_1_KJH.py

---
+++ Day_7/Arrays_1_KJH.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    arr.reverse()
+    for n in arr:
+        print(n, end=" ")
