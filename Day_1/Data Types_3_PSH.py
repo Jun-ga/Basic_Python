4d631162d216b607342fa301dc8ec3a310e6d437
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2020-01-07 16:22:13 +0900

    Create Data Types_3_PSH.py

---
+++ Day_1/Data Types_3_PSH.py
@@ -0,0 +1,7 @@
+   int i2 = scan.nextInt();
+        double d2 = scan.nextDouble();
+        scan.nextLine();
+        String s2 = scan.nextLine();
+        System.out.println(i+i2);
+        System.out.println(d+d2);
+        System.out.println(s+s2);
