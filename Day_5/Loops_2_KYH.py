3c3dee07ae1fb5066407ff9b744a87b220f79749
gek7537 <56713634+gek7537@users.noreply.github.com>
2020-01-07 14:06:08 +0900

    Create Loops_2_KYH.py

---
+++ Day_5/Loops_2_KYH.py
@@ -0,0 +1,12 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,11):
+        t=n*i
+        print(n,"x",i,"=",t)
