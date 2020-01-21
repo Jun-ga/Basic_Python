b8011ec767c073abe97dca02af90348081cc7ffa
jb0511 <59651113+jb0511@users.noreply.github.com>
2020-01-21 13:23:03 +0900

    Create Inheritance_2_KJB.py

---
+++ Day_12/Inheritance_2_KJB.py
@@ -0,0 +1,39 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person): 
+    def __init__(self, firstName, lastName, idNumber,scores):
+        Person.__init__(self, firstName, lastName, idNumber)
+        self.scores = scores
+
+    def calculate(self):
+        avg = sum(self.scores) / len(self.scores)
+        if avg >= 90: 
+            return 'O'
+        elif avg >= 80:
+             return 'E'
+        elif avg >= 70:
+             return 'A'
+        elif avg >= 55:
+             return 'P'
+        elif avg >= 40:
+             return 'D'
+        else :
+            return 'T'
+
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
