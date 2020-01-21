046e3a6c432064109fb78f6b1f6fbd5c1e136cad
kwonjunhee <55957124+kwonjunhee@users.noreply.github.com>
2020-01-21 16:51:39 +0900

    Update Dictionaries and Maps_1_KJH.py

--- Day_08/Dictionaries and Maps_1_KJH.py
+++ Day_08/Dictionaries and Maps_1_KJH.py
@@ -13,3 +13,24 @@ for _ in range(n):
         print (name+"="+ number)
     else:
         print ("Not found")
+        
+        
+# 고친 코드
+n=int(input())
+phoneBook={}
+
+ 
+for _ in range(n):
+    name, number = input().split()
+    phoneBook[name]=number
+
+for _ in range(n):
+    try:
+        name=input()
+        if name in phoneBook:
+            number=phoneBook[name]
+            print (name+"="+ number)
+        else:
+            print ("Not found")
+    except:
+        break
