a6226f8df73c311b71f3786cc01ee170c940c820
jb0511 <59651113+jb0511@users.noreply.github.com>
2020-01-21 13:00:45 +0900

    Create Binary Numbers_2_KJB.py

---
+++ Day_10/Binary Numbers_2_KJB.py
@@ -0,0 +1,12 @@
+
+if __name__ == '__main__':
+    n = int(input())
+    cnt = 0
+    mx = 0
+    while n > 0:
+        if n%2 == 1:
+            cnt += 1
+            if cnt > mx: mx = cnt
+        else: cnt = 0
+        n //= 2
+    print( mx )
