319e63ff2ce064d7322ed3b0cba616fa7950ff62
jb0511 <59651113+jb0511@users.noreply.github.com>
2020-01-28 14:23:00 +0900

    Create Finding the percentage_2_KJB.py

---
+++ [1월4주차]/Basic Data Types/Finding the percentage/Finding the percentage_2_KJB.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        scores = sum(scores)/3
+        student_marks[name] = scores #해석
+    query_name = input()
+    print('%.2f'%student_marks[query_name])#해석
+
