65b23b109ab70d4c96bdb3e437b25b9706452b94
kwonjunhee <55957124+kwonjunhee@users.noreply.github.com>
2020-01-28 17:29:09 +0900

    Update Tuples_1_KJH.py

--- [1월4주차]/Basic Data Types/Tuples/Tuples_1_KJH.py
+++ [1월4주차]/Basic Data Types/Tuples/Tuples_1_KJH.py
@@ -2,3 +2,7 @@ if __name__ == '__main__':
     n = int(input())
     integer_list = tuple(map(int, input().split()))
     print(hash(integer_list))
+'''
+if __name__ == '__main__':
+    n = int(input())
+    print(hash(tuple(map(int, input().split())))) '''
