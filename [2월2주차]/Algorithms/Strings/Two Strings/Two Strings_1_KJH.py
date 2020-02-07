26ecc5b4cb4a0c981ff4e157ccd62860707a9889
kwonjunhee <55957124+kwonjunhee@users.noreply.github.com>
2020-02-07 16:13:08 +0900

    Create Two Strings_1_KJH.py

---
+++ [2월2주차]/Algorithms/Strings/Two Strings/Two Strings_1_KJH.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+def twoStrings(s1, s2):
+    if any(i in s2 for i in s1 ): return "YES"
+    else: return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
