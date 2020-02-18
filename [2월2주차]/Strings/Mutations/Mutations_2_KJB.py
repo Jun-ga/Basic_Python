2c0158cfd8593ba578ddd42cf07f2437d520d539
jb0511 <59651113+jb0511@users.noreply.github.com>
2020-02-18 14:48:32 +0900

    Create Mutations_2_KJB.py

---
+++ [2월2주차]/Strings/Mutations/Mutations_2_KJB.py
@@ -0,0 +1,9 @@
+def mutate_string(s, p, c):
+    return s[:p] + c + s[p+1:]
+
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
