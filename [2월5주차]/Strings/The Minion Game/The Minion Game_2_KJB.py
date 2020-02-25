ab79b3a5744161a945a58ae022e61beab5d6c168
gek7537 <56713634+gek7537@users.noreply.github.com>
2020-02-25 17:31:11 +0900

    Update The Minion Game_2_KJB.py

--- [2월5주차]/Strings/The Minion Game/The Minion Game_2_KJB.py
+++ [2월5주차]/Strings/The Minion Game/The Minion Game_2_KJB.py
@@ -1,4 +1,4 @@
-def minion_game(string):
+'''def minion_game(string):
     s = raw_input()
 
 vowels = 'AEIOU'
@@ -20,4 +20,37 @@ else:
 
 if __name__ == '__main__':
     s = raw_input()
+    minion_game(s)
+'''
+#수정
+def minion_game(string):
+    # your code goes here
+    s = string
+    S = 0
+    K =0
+
+    vowels = 'AEIOU'
+
+
+    for i in range(len(s)):
+        if s[i] in vowels:
+            K += (len(s)-i) #쓰는방식이 맞나...
+        else:
+            S += (len(s)-i)
+
+    if K > S:
+        print ("Kevin", end='')
+        print(" ", end='')
+        print(K)
+        
+    elif K < S:
+        print ("Stuart", end='')
+        print(" ", end='')
+        print(S)
+    else:
+        print ("Draw")
+    # your code goes here
+
+if __name__ == '__main__':
+    s = input()
     minion_game(s)
