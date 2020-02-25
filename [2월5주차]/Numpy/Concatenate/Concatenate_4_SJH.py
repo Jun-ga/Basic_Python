bc6ed4159e8911c535085143a5cfa21d55754cb4
songjh217 <56715366+songjh217@users.noreply.github.com>
2020-02-25 16:52:30 +0900

    Update Concatenate_4_SJH.py

--- [2월5주차]/Numpy/Concatenate/Concatenate_4_SJH.py
+++ [2월5주차]/Numpy/Concatenate/Concatenate_4_SJH.py
@@ -5,15 +5,17 @@ arr = numpy.array(A, int)
 list_1 = []
 list_2 = []
 
-for _ in range (arr[0]-1):
+for _ in range (arr[0]):
     B = input().strip().split()
     list_1.append(B)
 
-for arr[0] in range (arr[1]+1):
+for _ in range (arr[1]):
     C = input().strip().split()
     list_2.append(C)
 
 array_1 = numpy.array(list_1, int)
 array_2 = numpy.array(list_2, int)
+#print(array_1)
+#print(array_2)
 
 print (numpy.concatenate((array_1, array_2), axis = 0)) 
